3b11b317bb690abfa69315672ca9d37e340f987a
ishishov <iuri.shishov@gmail.com>
2017-11-09 23:37:26 +0100

    Integrate psychogram

--- app/controllers/ProfileController.scala
+++ app/controllers/ProfileController.scala
@@ -4,10 +4,10 @@ import javax.inject.Inject
 
 import com.mohiva.play.silhouette.api._
 import forms.ProfileForm
-import models.enums.Language
 import models.services.UserService
 import org.webjars.play.WebJarsUtil
-import play.api.i18n.I18nSupport
+import play.api.i18n._
+import play.api.libs.json.Json
 import play.api.mvc.{ AbstractController, ControllerComponents }
 import utils.auth.DefaultEnv
 
@@ -22,10 +22,29 @@ class ProfileController @Inject() (
   userService: UserService,
   assets: AssetsFinder,
   ex: ExecutionContext
+
 ) extends AbstractController(components) with I18nSupport {
 
   def view = silhouette.SecuredAction.async { implicit request =>
-    userService.retrieveProfile(request.identity.userID).map(p => Ok(views.html.profile(request.identity, p)))
+
+    userService.retrieveProfile(request.identity.userID).map {
+      case Some(p) =>
+        val messages: Messages = request.messages
+        val map = Map[String, List[(String, Int)]]()
+
+        val interests = "interests" -> List((messages("interest." + p.interest1), p.timeInterest1), (messages("interest." + p.interest2), p.timeInterest2), (messages("interest." + p.interest3), p.timeInterest3))
+        val surroundings = "surroundings" -> List(("", 0), ("", 0), ("", 0), ("", 0))
+        val constitution = "constitution" -> List(("", 0), ("", 0), ("", 0), ("", 0))
+        val expertise = "expertise" -> List((messages("subject." + p.subjectGoodAt), p.scoreSubjectGoodAt), (messages("subject." + p.subjectGoodAt), p.scoreSubjectGoodAt), (messages("subject." + p.subjectImprove), p.scoreSubjectImprove), (messages("subject." + p.subjectImprove), p.scoreSubjectImprove))
+        val operation = "operation" -> List(("", 0), ("", 0), ("", 0))
+
+        val psychogramDataJson = Json.stringify(Json.toJson(map + interests + surroundings + constitution + expertise + operation))
+
+        Ok(views.html.profile(request.identity, Some(p), psychogramDataJson))
+      case None => Ok(views.html.profile(request.identity, None, "{}"))
+
+    }
+
   }
 
   def edit = silhouette.SecuredAction.async { implicit request =>
@@ -42,8 +61,8 @@ class ProfileController @Inject() (
 
     Future.successful(ProfileForm.form.bindFromRequest.fold(
       errors =>
-        //Redirect(routes.ApplicationController.index).flashing("error" -> s"Error"),
-        Redirect(routes.ProfileController.edit).flashing("error" -> s"Error"),
+        //Redirect(routes.ApplicationController.index).flashing("error" -> s"Error: $errors"),
+        Redirect(routes.ProfileController.edit).flashing("error" -> s"Error: ${errors.toString}"),
       profileSuccess => {
         val profile = profileSuccess.copy(userID = request.identity.userID)
         userService.saveProfile(profile)
--- public/javascripts/psychogram.js
+++ public/javascripts/psychogram.js
@@ -1,29 +1,32 @@
-$(function() {
+function applyPsychogram(psychogramData) {
+
     var criticalValueColor = "#f57888";
     var fineValueColor = "#b9e88b";
     var criticalLevel = 1;
 
     var scores = {
-        specialist: [1, 1, 4, 5],
-        environment: [1, 1, 2, 2],
-        constitution: [3, 1, 2, 2],
-        interests: [2, 2, 4],
-        workmode: [5, 2, 1, 0],
+        expertise: psychogramData["expertise"],
+        surroundings: psychogramData["surroundings"],
+        constitution: psychogramData["constitution"],
+        interests: psychogramData["interests"],
+        operation: psychogramData["operation"]
     };
 
     var categoryNameMapping = {
-        specialist: "s",
-        environment: "e",
+        expertise: "e",
+        surroundings: "s",
         constitution: "c",
         interests: "i",
-        workmode: "w"
+        operation: "o"
     };
 
     for (var categoryName in scores) {
         var scoreList = scores[categoryName];
         for (var ind = 0; ind < scoreList.length; ind++) {
-            var score = scoreList[ind];            
+            var subcategoryName = scoreList[ind][0].toUpperCase();
+            var score = scoreList[ind][1];
             renderSubcategoryScore(categoryName, ind, score);
+            applySubcategoryText(categoryName, ind, subcategoryName);
         }
     }
 
@@ -33,13 +36,18 @@ $(function() {
             var color = score > criticalLevel ? fineValueColor : criticalValueColor;
             for (var counter = 0; counter < affectedCells; counter++) {
                 var cellId = getCellId(categoryName, subcategoryIndex, 5 - counter);
-                var cell = document.getElementById(cellId);
-                if (cell) cell.setAttribute("fill", color);
+                var cell = $('#' + cellId);
+                if (cell) cell.attr("fill", color);
             }
-        }        
+        }
     }
 
     function getCellId(categoryName, subcategoryIndex, cellIndex) {
         return categoryNameMapping[categoryName] + "-" + subcategoryIndex + "-" + cellIndex;
     }
-})();
+
+    function applySubcategoryText(categoryName, subcategoryIndex, subcategoryName) {
+        var cellId = categoryNameMapping[categoryName] + "-" + subcategoryIndex;
+        $('#' + cellId + ' tspan').text(subcategoryName);
+    }
+}
