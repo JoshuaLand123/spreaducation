618d25c48b377f976adda8690f31672892a6ce7a
ishishov <iuri.shishov@gmail.com>
2017-12-27 11:48:29 +0100

    Activate email validation for tutees

--- app/controllers/SignUpController.scala
+++ app/controllers/SignUpController.scala
@@ -9,16 +9,16 @@ import com.mohiva.play.silhouette.api.services.AvatarService
 import com.mohiva.play.silhouette.api.util.PasswordHasherRegistry
 import com.mohiva.play.silhouette.impl.providers._
 import forms.SignUpForm
-import models.services.{ AuthTokenService, UserService }
+import models.services.{AuthTokenService, UserService}
 import models.User
 import models.enums.UserType
 import org.webjars.play.WebJarsUtil
-import play.api.i18n.{ I18nSupport, Messages }
-import play.api.libs.mailer.{ Email, MailerClient }
-import play.api.mvc.{ AbstractController, AnyContent, ControllerComponents, Request }
+import play.api.i18n.{I18nSupport, Messages}
+import play.api.libs.mailer.{Email, MailerClient}
+import play.api.mvc.{AbstractController, AnyContent, ControllerComponents, Request}
 import utils.auth.DefaultEnv
 
-import scala.concurrent.{ ExecutionContext, Future }
+import scala.concurrent.{ExecutionContext, Future}
 
 /**
  * The `Sign Up` controller.
@@ -69,24 +69,18 @@ class SignUpController @Inject() (
     SignUpForm.form.bindFromRequest.fold(
       form => Future.successful(BadRequest(views.html.signUp(form))),
       data => {
-        val result =
-          if (data.userType == UserType.Tutor)
-            Redirect(routes.SignInController.view()).flashing("info" -> Messages("sign.up.email.sent", data.email))
-          else
-            Redirect(routes.SignInController.view())
+        val result = Redirect(routes.SignInController.view()).flashing("info" -> Messages("sign.up.email.sent", data.email))
         val loginInfo = LoginInfo(CredentialsProvider.ID, data.email)
         userService.retrieve(loginInfo).flatMap {
           case Some(user) =>
-            if (data.userType == UserType.Tutor) {
-              val url = routes.SignInController.view().absoluteURL()
-              mailerClient.send(Email(
-                subject = Messages("email.already.signed.up.subject"),
-                from = Messages("email.from"),
-                to = Seq(data.email),
-                bodyText = Some(views.txt.emails.alreadySignedUp(user, url).body),
-                bodyHtml = Some(views.html.emails.alreadySignedUp(user, url).body)
-              ))
-            }
+            val url = routes.SignInController.view().absoluteURL()
+            mailerClient.send(Email(
+              subject = Messages("email.already.signed.up.subject"),
+              from = Messages("email.from"),
+              to = Seq(data.email),
+              bodyText = Some(views.txt.emails.alreadySignedUp(user, url).body),
+              bodyHtml = Some(views.html.emails.alreadySignedUp(user, url).body)
+            ))
 
             Future.successful(result)
           case None =>
@@ -100,7 +94,7 @@ class SignUpController @Inject() (
               email = Some(data.email),
               avatarURL = None,
               userType = data.userType,
-              activated = if (data.userType == UserType.Tutee) true else false
+              activated = false
             )
             for {
               avatar <- avatarService.retrieveURL(data.email)
@@ -108,16 +102,14 @@ class SignUpController @Inject() (
               authInfo <- authInfoRepository.add(loginInfo, authInfo)
               authToken <- authTokenService.create(user.userID)
             } yield {
-              if (data.userType == UserType.Tutor) {
-                val url = routes.ActivateAccountController.activate(authToken.id).absoluteURL()
-                mailerClient.send(Email(
-                  subject = Messages("email.sign.up.subject"),
-                  from = Messages("email.from"),
-                  to = Seq(data.email),
-                  bodyText = Some(views.txt.emails.signUp(user, url).body),
-                  bodyHtml = Some(views.html.emails.signUp(user, url).body)
-                ))
-              }
+              val url = routes.ActivateAccountController.activate(authToken.id).absoluteURL()
+              mailerClient.send(Email(
+                subject = Messages("email.sign.up.subject"),
+                from = Messages("email.from"),
+                to = Seq(data.email),
+                bodyText = Some(views.txt.emails.signUp(user, url).body),
+                bodyHtml = Some(views.html.emails.signUp(user, url).body)
+              ))
 
               silhouette.env.eventBus.publish(SignUpEvent(user, request))
               result
