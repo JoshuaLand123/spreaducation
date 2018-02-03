892e2283cac05ea45949a61cf3bb9874b88b7b96
ishishov <iuri.shishov@gmail.com>
2018-02-03 21:01:26 +0100

    Fix languages

--- app/models/enums/Language.scala
+++ app/models/enums/Language.scala
@@ -6,9 +6,9 @@ object Language extends Enumeration {
   type Language = Value
   val Afrikaans, Albanian, Amharic, Arabic, Aramaic, Armenian, Assamese, Aymara, Azerbaijani, Balochi, Bamanankan,
   BashkortBashkir, Basque, Belarusan, Bengali, Bhojpuri, Bislama, Bosnian, Brahui, Bulgarian, Burmese, Cantonese,
-  Catalan, Cebuano, Chechen, Cherokee, Chinese, Croatian, Czech, Dakota, Danish, Dari, Dholuo, DutchEnglish, Esperanto,
-  Estonian, Éwé, Finnish, French, Georgian, German, Gikuyu, Greek, Guarani, Gujarati, HaitianCreole, Hausa, Hawaiian,
-  HawaiianCreole, Hebrew, Hiligaynon, Hindi, Hungarian, Icelandic, Igbo, Ilocano, Indonesian, InuitOrInupiaq,
+  Catalan, Cebuano, Chechen, Cherokee, Chinese, Croatian, Czech, Dakota, Danish, Dari, Dholuo, Dutch, English,
+  Esperanto, Estonian, Ewe, Finnish, French, Georgian, German, Gikuyu, Greek, Guarani, Gujarati, HaitianCreole, Hausa,
+  Hawaiian, HawaiianCreole, Hebrew, Hiligaynon, Hindi, Hungarian, Icelandic, Igbo, Ilocano, Indonesian, InuitOrInupiaq,
   IrishGaelic, Italian, Japanese, Jarai, Javanese, Kiche, Kabyle, Kannada, Kashmiri, Kazakh, Khmer, KhoekhoeKorean,
   Kurdish, Kyrgyz, Lao, Latin, Latvian, Lingala, Lithuanian, Macedonian, Maithili, Malagasy, Malay, Malayalam, Mandarin,
   Marathi, Mende, Mongolian, Nahuatl, Navajo, Nepali, Norwegian, Ojibwa, Oriya, Oromo, Pashto, Persian, Polish,
