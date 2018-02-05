3a1b511f3fa82d6f523ded8e3441694ee0aaa561
ishishov <iuri.shishov@gmail.com>
2018-02-05 21:30:46 +0100

    update evolutions

--- conf/evolutions/default/19.sql
+++ conf/evolutions/default/19.sql
@@ -1,9 +1,9 @@
 # --- !Ups
 
 ALTER TABLE events DROP title;
-UPDATE TABLE events SET event_type = 'Available' WHERE event_type = 'Availability';
+UPDATE events SET event_type = 'Available' WHERE event_type = 'Availability';
 
 # --- !Downs
 
 ALTER TABLE events ADD title text;
-UPDATE TABLE events SET event_type = 'Availability' WHERE event_type = 'Available';
+UPDATE events SET event_type = 'Availability' WHERE event_type = 'Available';
